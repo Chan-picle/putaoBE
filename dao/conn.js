6d82dcb04405d5e9765df304060e5f96e32b1379
Chan-picle <53422466+Chan-picle@users.noreply.github.com>
2020-12-24 11:45:59 +0800

    Update conn.js

--- dao/conn.js
+++ dao/conn.js
@@ -1,9 +1,7 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '42.192.148.152',
-  user: 'putaojia',
-  password: '123456',
+ 
   database: 'putaojia',
   port:3306
 });
