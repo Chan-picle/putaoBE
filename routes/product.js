1357d971ae1a6ef256bf1844bc8a2fcf642e9814
Chen JiaYing <chenpickle@163.com>
2020-12-28 22:42:59 +0800

    登陆注册接口

--- routes/product.js
+++ routes/product.js
@@ -22,7 +22,7 @@ router.get("/productlist",function(req,res,next){
 // 通过产品id获取数据
 router.post("/getproduct",function(req,res,next){
   // req.body.id
-  let sqlSearch = `select * from course_list where productId ="${req.body.id}"`;
+  let sqlSearch = `select * from index_banner,course_list where course_list.t_name = index_banner.t_name and course_list.productId = "${req.body.id}"`;
   conn.query(sqlSearch, (err, results) => {
     let obj = {
       status:1,
@@ -37,5 +37,23 @@ router.post("/getproduct",function(req,res,next){
     res.json(obj);
   })
 });
-
+// select * from index_banner,course_list where course_list.t_name = index_banner.t_name and index_banner.t_no= 1
+// 通过teacher_id获取数据
+router.post("/teachercourse",function(req,res,next){
+  // req.body.id
+  let sqlSearch = `select * from index_banner,course_list where course_list.t_name = index_banner.t_name and index_banner.t_no = "${req.body.id}"`;
+  conn.query(sqlSearch, (err, results) => {
+    let obj = {
+      status:1,
+    }
+    if (err) {
+      obj.msg = "失败"
+      throw err
+    }else{
+      obj.status = 0;
+      obj.result = results;
+    }
+    res.json(obj);
+  })
+});
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,98 @@
 const express = require('express');
 const conn = require('../dao/conn.js');
+const axios = require("axios");
 
 const router = express.Router();
 
-router.get('/getuser', function(req, res, next) {
+//获取所有用户信息
+router.get("/getuser",function(req,res,next){
   let sqlSearch = `select * from user`;
   conn.query(sqlSearch, (err, results) => {
-      if (err) throw err;
-      res.json(results);
+      let obj = {
+        status:1
+      }
+      if (err) {
+        obj.msg = "失败"
+        throw err
+      }else{
+        obj.status=0;
+        obj.result = results;
+      }
+      res.json(obj);
   })
 });
+
+router.post('/sendcode', function(req, res, next) {
+  let revalue={status:1};
+  //使用别的接口发送短信
+  axios.post("http://www.pudge.wang:3180/register/getCode",
+  {
+    phone:req.body.phone,
+    templateId:"537707"
+  }).then(result=>{
+    revalue.status = 0;
+    revalue.msg = result.data.status == 0 ? "验证码已发送":result.data.msg;
+    res.json(revalue);
+  }).catch(err=>{
+    res.json(revalue);
+    throw err;
+  })
+});
+//登陆 {phone:"123123123",code:"55555"}
 router.post('/login', function(req, res, next) {
-  //电话号码正则判断
-  let sqlSearch = `select * from user where user_phone=${req.body.phone}`;
-  conn.query(sqlSearch, (err, results) => {
-      if (err) throw err;
-      // res.json(results);
-      console.log(results);
-  })
-});
+  let revalue = {status:1,result:{userid:''}};
+  axios.post("http://www.pudge.wang:3180/register",
+  {
+    phone:req.body.phone,
+    code:req.body.code
+  }).then(result=>{//result 为pudge接口返回数据
+    // console.log(result.data)
+    if(result.data.status==0){
+      //成功,将用户phone和数据库比对
+      revalue.status = 0;
+      let sql = `select * from user where  user_phone = "${req.body.phone}" `;
+      conn.query(sql, (err, results) => {//results 为数据库查询结果
+        if(err)throw err;
+        if(results.length){
+          //用户已存在，登陆
+          revalue.msg = "登陆成功，即将跳转";
+          let d = new Date();
+          let updateSql = `UPDATE user SET login_time="${d.toLocaleString()}" WHERE user_phone="${req.body.phone}"`;
+          conn.query(updateSql,(e,r)=>{
+            if(e)throw e;
+            revalue.result = result.data.result;
+            revalue.result.userid = results[0].user_id;
+            res.json(revalue);
+          });
+        }else{
+          //用户不存在，注册
+          revalue.msg = "注册成功，即将跳转";
+          let d = new Date();
+          let randomName = function(){
+            let str = "用户";
+            for(let i=0;i<10;i++){
+              str+=Math.round(Math.random()*10)
+            }
+            return str;
+          };
+          let insertSql = `INSERT INTO user (user_name, user_password,user_phone,login_time) VALUES ("${randomName()}" , "${result.data.result.token}","${req.body.phone}","${d.toLocaleString()}");`;
+          conn.query(insertSql,(e,r)=>{
+            if(e)throw e;
+            revalue.result = result.data.result;
+            revalue.result[userid] = r.insertId;
+            res.json(revalue);
+          })
+        }
+    })
+    }
+    else{
+      //pudge接口返回错误status
+      revalue.msg = result.data.msg;
+      res.json(revalue);
+    }
+  }).catch(e=>{throw e});
+})
 module.exports = router;
 
 
 
-// let insertSql = `insert into user (user_name,user_password,user_phone) 
-//     values ('chen123','123','8888888888')`;
-// conn.query(insertSql, (err, results) => { //results是结果数组
-//   if (err) throw err;
-//   console.log(results)
-// })
