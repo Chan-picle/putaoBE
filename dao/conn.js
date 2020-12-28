c1bd5f99df7d8ee747bf9752a93b6c623ec9e006
Chen JiaYing <chenpickle@163.com>
2020-12-28 22:47:44 +0800

    1

--- dao/conn.js
+++ dao/conn.js
@@ -1,7 +1,9 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 10,
- 
+  host: '42.192.148.152',
+  user: 'putaojia',
+  password: '123456',
   database: 'putaojia',
   port:3306
 });
